a4ba7893799d1b15a790c954c4c7ffafabd54c68
OmarThierno <thiernomarndiaye76@gmail.com>
2024-04-02 18:27:46 +0200

    feat: fix repetition

--- Snack 5/js/script.js
+++ Snack 5/js/script.js
@@ -2,29 +2,16 @@
 const numOdd = [];
 const numUser = [];
 
-const num1 = parseInt(prompt('Inserisci un numero!'));
-const num2 = parseInt(prompt('Inserisci un numero!'));
-const num3 = parseInt(prompt('Inserisci un numero!'));
-const num4 = parseInt(prompt('Inserisci un numero!'));
-const num5 = parseInt(prompt('Inserisci un numero!'));
-const num6 = parseInt(prompt('Inserisci un numero!'));
-
-numUser.push(num1);
-numUser.push(num2);
-numUser.push(num3);
-numUser.push(num4);
-numUser.push(num5);
-numUser.push(num6);
-
 console.log(numUser);
 
-for (let i = 0; i < numUser.lenght; i++) {
-  if (numUser[i] % 2 !== 0) {
-    numOdd.push(numUser[i]);
-    console.log(numOdd);
-    console.log(numUser);
+for (let i = 0; i < 6; i++) {
+  const num = parseInt(prompt('Inserisci un numero!'));
+  numUser.push(num);
+  if (num % 2 !== 0) {
+    numOdd.push(num);
   }
   
 }
 
-console.log(numOdd);
+console.log('Numeri inseriti',numUser);
+console.log('Numeri dispari',numOdd);
--- Snack 8/js/script.js
+++ Snack 8/js/script.js
@@ -1,17 +1,18 @@
-const num1 = parseInt(prompt('Inserisci un numero!'));
-const num2 = parseInt(prompt('Inserisci un numero!'));
-const num3 = parseInt(prompt('Inserisci un numero!'));
-const num4 = parseInt(prompt('Inserisci un numero!'));
+const userNum = parseInt(prompt('Inserisci un numero!'));
 
+const num = [];
 let sum = 0;
 
-if (!isNaN(num1) && num1 > 0 && !isNaN(num2) && num2 > 0 && !isNaN(num3) && num3 > 0 && !isNaN(num4) && num4 > 0) {
-  sum += num1;
-  sum += num2;
-  sum += num3;
-  sum += num4;
-  
+if (userNum > 999 && userNum < 9999) {
+  for (let i = 0; i < userNum.toString().length; i++) {
+    console.log(userNum)
+    num.push(userNum.toString()[i]);
+  }
+  console.log(num)
+  for (let i = 0; i < num.length; i++) {
+    sum += parseInt(num[i]);
+  }
   console.log(sum);
 } else {
-  console.log('non sono accettati numeri negativi!')
+  console.log('inserisci un numero di quatro cifre')
 }
